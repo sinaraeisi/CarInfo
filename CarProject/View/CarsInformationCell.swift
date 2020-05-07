789e3495336e63b35c2a310961c32a057a1e7efe
sina raeisi <shndrs@sahands-MacBook-Pro.local>
2020-05-07 13:57:11 +0430

    Initial commit

---
+++ CarProject/View/CarsInformationCell.swift
@@ -0,0 +1,24 @@
+//
+//  CarsInformationCell.swift
+//  CarProject
+//
+//  Created by sahand raeisi on 5/7/20.
+//  Copyright © 2020 sina raeisi. All rights reserved.
+//
+
+import UIKit
+
+class CarsInformationCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CarProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarProject
+//
+//  Created by sahand raeisi on 5/7/20.
+//  Copyright © 2020 sina raeisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
